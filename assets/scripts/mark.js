e62852adb69af2b5e25c6ecb979cbfc40518b60f
ReyPena <zonickblack@gmail.com>
2016-08-04 18:53:59 -0600

    Fix background size.

---
+++ assets/scripts/mark.js
@@ -0,0 +1,19 @@
+console.log(
+    ".------------------." + "\n" +
+    "| .--------------. |" + "\n" +
+    "| |  _______     | |" + "\n" +
+    "| | |_   __ \\    | |" + "\n" +
+    "| |   | |__) |   | |" + "\n" +
+    "| |   |  __ /    | |" + "\n" +
+    "| |  _| |  \\ \\_  | |" + "\n" +
+    "| | |____| |___| | |" + "\n" +
+    "| |              | |" + "\n" +
+    "| '--------------' |" + "\n" +
+    "'------------------'" + "\n" +
+    "|  email: zonickblack@gmail.com                   |" + "\n" +
+    "|  web-site: http://www.reypena.tech/             |" + "\n" +
+    "|  inkedin: https://www.linkedin.com/in/reypena   |" + "\n" +
+    "|  github: https://github.com/ReyPena             |" + "\n" + "\n" +
+    "Hello my name is Rey im a web developer, and a passionate designer who is enthusiastic about embracing new technologies and carefully crafting elegant web applications." + "\n" +
+    "I'm an autodidactic person who is independent, looking for an opportunity to gain employment with a company or institution that offers me a consistently positive atmosphere to learn and implement new technologies for the betterment of myself and the organization."
+);
